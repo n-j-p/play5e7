75c48e7e751d2972cd499a6dd4966eb7d6d3b854
n-j-p <10066183+n-j-p@users.noreply.github.com>
2025-07-24 08:52:38 +1000

    working

--- play5e7/dataprep.py
+++ play5e7/dataprep.py
@@ -1,13 +1,15 @@
-import polars as pl
+#import polars as pl
 import pandas as pd
 import os
 from collections import namedtuple
 
 def load_data(data_path = '/kaggle/input/playground-series-s5e7'):
-    train = pl.read_csv(os.path.join(data_path, 'train.csv'))
+    train = pd.read_csv(os.path.join(data_path, 'train.csv'),
+                        index_col='id')
 
-    comp = pl.read_csv(os.path.join(data_path, 'test.csv'))
-    sub = pl.read_csv(os.path.join(data_path, 'sample_submission.csv'))
+    comp = pd.read_csv(os.path.join(data_path, 'test.csv'),
+                       index_col='id')
+    sub = pd.read_csv(os.path.join(data_path, 'sample_submission.csv'))
     Data = namedtuple('Data', ['train',
                                'comp',
                                'sub'])
@@ -16,3 +18,12 @@ def load_data(data_path = '/kaggle/input/playground-series-s5e7'):
                 sub)
 
 
+from sklearn.pipeline import Pipeline
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.preprocessing import OrdinalEncoder
+#oe = OrdinalEncoder().set_output(transform='pandas')
+pipe = Pipeline([('encode', OrdinalEncoder().set_output(transform='pandas')),
+                 ('impute', IterativeImputer(max_iter=100))
+                ]).set_output(transform='pandas')
+                
